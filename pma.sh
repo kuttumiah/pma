2a51961e3438bf11c4ed032c002070dd9d36a051
Robaiatul Islam <imkuttumiah@gmail.com>
2019-09-15 23:35:48 +0600

    Automate generation of phpMyAdmin config.
    - create phpMyAdmin config config.inc.php with minimal configuration
    - add .gitignore file to project

--- pma.sh
+++ pma.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 LATEST_VERSION=$(curl -sS 'https://api.github.com/repos/phpmyadmin/phpmyadmin/releases/latest' | awk -F '"' '/tag_name/{print $4}')
-DOWNLOAD_URL="https://api.github.com/repos/phpmyadmin/phpmyadmin/tarball/$LATEST_VERSION"
+DOWNLOAD_URL="https://api.github.com/repos/phpmyadmin/phpmyadmin/tarball/${LATEST_VERSION}"
 
-echo "Downloading phpMyAdmin $LATEST_VERSION"
+echo "Downloading phpMyAdmin ${LATEST_VERSION}"
 wget $DOWNLOAD_URL -q --show-progress -O 'phpmyadmin.tar.gz'
 
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
@@ -12,8 +12,10 @@ rm phpmyadmin.tar.gz
 
 CMD=/vagrant/scripts/site-types/laravel.sh
 CMD_CERT=/vagrant/scripts/create-certificate.sh
+PMACONFIGFILE="$(pwd)/phpmyadmin/config.inc.php"
+RANDOMBLOWFISHSECRET=$(openssl rand -base64 32)
 
-if [ ! -f $CMD ]; then
+if [[ ! -f $CMD ]]; then
     # Fallback for older Homestead versions
     CMD=/vagrant/scripts/serve.sh
 else
@@ -25,5 +27,11 @@ sudo bash $CMD phpmyadmin.test $(pwd)/phpmyadmin 80 443 7.3
 
 echo "Installing dependencies for phpMyAdmin"
 cd phpmyadmin && composer update --no-dev
+
+echo 'Creating a new config.inc.php file...'
+cp "$(pwd)/config.sample.inc.php" $PMACONFIGFILE
+
+# setup blowfish
+sed -i -e "/blowfish_secret/ s:=.*:= '${RANDOMBLOWFISHSECRET}';:" $PMACONFIGFILE
 
 sudo service nginx reload
